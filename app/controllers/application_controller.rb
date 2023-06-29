6a5ef6cbcc34a420df44754086d36bc5150ee604
veronica365 <naruth10@gmail.com>
2023-06-29 19:36:43 +0300

    Add functionality to show food by id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :_private_record_not_found
 
   protected
 
@@ -11,6 +12,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :password)
+    end
+  end
+
+  def _private_record_not_found
+    respond_to do |format|
+      format.html { render 'shared/_404' }
+      format.json { render json: { errors: 'The details no longer exist' }, status: :not_found }
     end
   end
 end
